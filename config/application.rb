324626f82cb6dd551d76437d7b85ffff24eb2dec
Michael Haapaniemi <michael.haapaniemi@gmail.com>
2012-08-25 16:48:57 -0500

    fix assets.precompile

--- config/application.rb
+++ config/application.rb
@@ -55,7 +55,7 @@ module SlushFund
 
     # Enable the asset pipeline
     config.assets.enabled = true
-    config.assest.precompile = true
+    config.assets.precompile = true
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
