2a7f4d949cba86afa4a87143d4ea9916d1043ec8
Michael Haapaniemi <michael.haapaniemi@gmail.com>
2012-09-06 21:34:15 -0500

    More styling

--- app/models/fund.rb
+++ app/models/fund.rb
@@ -6,6 +6,16 @@ class Fund < ActiveRecord::Base
   has_many :requests
   
   validates_presence_of :deadline, :budget, :name, :party_time, :host, :location, :description
+  validate :time_after_today
+  validates :budget, :numericality => true
+  
+  def time_after_today
+    if party_time < DateTime.now || deadline < DateTime.now
+      errors.add(:party_time, "must be after today")
+      errors.add(:deadline, "must be after today")
+    end
+  end
+
   
   mount_uploader :avatar , AvatarUploader
 end
