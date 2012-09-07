87cc91f06c96bf51883c33a47353977b9d770e28
Michael Haapaniemi <michael.haapaniemi@gmail.com>
2012-09-06 22:14:10 -0500

    Messing with migrations

--- db/migrate/20120821193610_create_requests.rb
+++ db/migrate/20120821193610_create_requests.rb
@@ -2,7 +2,7 @@ class CreateRequests < ActiveRecord::Migration
   def change
     create_table :requests do |t|
       t.string :item
-      t.string :user_id
+      t.integer :user_id
 
       t.timestamps
     end
