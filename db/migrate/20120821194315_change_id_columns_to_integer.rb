ee4dbc4119c02a6997fd27771eaaa6964d7e8f58
Michael Haapaniemi <michael.haapaniemi@gmail.com>
2012-09-06 22:12:09 -0500

    Changed migration

--- db/migrate/20120821194315_change_id_columns_to_integer.rb
+++ db/migrate/20120821194315_change_id_columns_to_integer.rb
@@ -1,10 +1,5 @@
 class ChangeIdColumnsToInteger < ActiveRecord::Migration
-  def self.up
+  def change
      change_column :requests, :user_id, :integer
-     
-    end
-
-    def self.down
-      change_column :requests, :user_id, :string
-    end
+   end
 end
